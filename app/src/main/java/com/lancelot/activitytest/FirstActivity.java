15e4e3f09ba6948b5fd686935559fd168e8d86d2
PanBinDev <bin.pan@foxmail.com>
2018-01-03 09:51:00 +0800

    添加Click Button触发Toast的功能

--- app/src/main/java/com/lancelot/activitytest/FirstActivity.java
+++ app/src/main/java/com/lancelot/activitytest/FirstActivity.java
@@ -2,6 +2,9 @@ package com.lancelot.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class FirstActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+
+        Button button1 =(Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(FirstActivity.this, "You clicked Button1", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
