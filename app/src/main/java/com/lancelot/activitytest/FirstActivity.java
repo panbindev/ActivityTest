bd14355c0efe0b61e717bb3e04df3230947ac008
PanBinDev <bin.pan@foxmail.com>
2018-01-04 09:38:42 +0800

    返回数据给上一个活动
    个人有改动，log改为Toast

--- app/src/main/java/com/lancelot/activitytest/FirstActivity.java
+++ app/src/main/java/com/lancelot/activitytest/FirstActivity.java
@@ -1,11 +1,12 @@
 package com.lancelot.activitytest;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.*;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -34,13 +35,16 @@ public class FirstActivity extends AppCompatActivity {
 //                intent.setData(Uri.parse("tel:10086"));
 //
 //
-            String data = "Hello SecondActivity";
+//            String data = "Hello SecondActivity";
             Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
-            intent.putExtra("extra_data",data);
-            startActivity(intent);
+//            intent.putExtra("extra_data",data);
+//            startActivity(intent);
 
+            startActivityForResult(intent,1);
             }
         });
+
+
 
 
     }
@@ -65,5 +69,19 @@ public class FirstActivity extends AppCompatActivity {
             default:
         }
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch(requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+                    String returnedData = data.getStringExtra("data_return");
+//                    Log.d("FirstActivity",returnedData);
+                    Toast.makeText(this,returnedData, Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+        }
     }
 }
--- app/src/main/java/com/lancelot/activitytest/SecondActivity.java
+++ app/src/main/java/com/lancelot/activitytest/SecondActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
@@ -13,10 +15,30 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Intent intent = getIntent();
-        String data =intent.getStringExtra("extra_data");
+//        Intent intent = getIntent();
+//        String data =intent.getStringExtra("extra_data");
+//
+////        Log.d("SecondActivity",data);
+//        Toast.makeText(SecondActivity.this,data,Toast.LENGTH_LONG).show();
 
-//        Log.d("SecondActivity",data);
-        Toast.makeText(SecondActivity.this,data,Toast.LENGTH_LONG).show();
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return","Hello First");
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed(){
+        Intent intent = new Intent();
+        intent.putExtra("data_return","Hello First");
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
