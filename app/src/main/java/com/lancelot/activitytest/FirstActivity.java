680ebf50218f430e180d08f4278412d322f97395
PanBinDev <bin.pan@foxmail.com>
2018-01-04 08:53:06 +0800

    向下一个活动传递数据
    个人改动：click button1而不是activity1

--- app/src/main/java/com/lancelot/activitytest/FirstActivity.java
+++ app/src/main/java/com/lancelot/activitytest/FirstActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.*;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,20 +21,28 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-//                Toast.makeText(FirstActivity.this, "You clicked Button1", Toast.LENGTH_SHORT).show();
-//                finish();
-//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
-//                Intent intent = new Intent("com.lancelot.activitytest.ACTION_START");
-//                intent.addCategory("com.lancelot.activitytest.MY_CATEGORY");
+////                Toast.makeText(FirstActivity.this, "You clicked Button1", Toast.LENGTH_SHORT).show();
+////                finish();
+////                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+////                Intent intent = new Intent("com.lancelot.activitytest.ACTION_START");
+////                intent.addCategory("com.lancelot.activitytest.MY_CATEGORY");
+//
+////                Intent intent = new Intent(Intent.ACTION_VIEW);
+////                intent.setData(Uri.parse("http://www.baidu.com"));
+//
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:10086"));
+//
+//
+            String data = "Hello SecondActivity";
+            Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+            intent.putExtra("extra_data",data);
+            startActivity(intent);
 
-//                Intent intent = new Intent(Intent.ACTION_VIEW);
-//                intent.setData(Uri.parse("http://www.baidu.com"));
-
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:10086"));
-                startActivity(intent);
             }
         });
+
+
     }
 
     @Override
--- app/src/main/java/com/lancelot/activitytest/SecondActivity.java
+++ app/src/main/java/com/lancelot/activitytest/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.lancelot.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String data =intent.getStringExtra("extra_data");
+        Log.d("SecondActivity",data);
     }
 }
